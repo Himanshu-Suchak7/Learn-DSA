f120988e06ab149bc3a052f2c9fdc7c6f2961fe0
Himanshu-Suchak7 <himanshusuchak.hs@gmail.com>
2024-07-20 11:17:45 +0530

    First Program, Data Types, Variables & Operators

---
+++ lecture2.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Hello World simple program
+    // cout << "Namaste Duniya my Name is Himasnhu" << endl;
+
+
+    // Variables
+    // int a = 123;
+    // cout << a << endl;
+    // char c = 'h';
+    // cout << c << endl;
+    // bool b = 1;
+    // cout << b << endl; //here we can write 1 or true for true values and 0 or false for false values 
+    // float f = 1.2;
+    // cout << f << endl;
+    // double d = 1.23;
+    // cout << d << endl;
+    // int size = sizeof(a); //output is 4 for integer
+    // // int size = sizeof(c); output is 1 for character
+    // // int size = sizeof(b); //output is 1 for boolean
+    // // int size = sizeof(f); //output is 4 for float
+    // // int size = sizeof(d); //output is 8 for double
+    // cout<<"Size of a is: "<<size<<endl;
+
+
+
+// Type Casting
+    // int a = 'a'; //Gives ascii value of a
+    // cout << a << endl;
+    // char b = 98; //Gives b as output because 98 is ascii value of b
+    // cout << b << endl;
+
+
+//How -ve number is represented
+    //-ve number = -5
+    // -ve number has starting digit as 1
+    // or just ignore the -ve sign
+    // or convert the number into binary representation
+    // or take the 2's complement of the -ve number
+    // int a = -5;
+    // cout << a << endl;
+
+
+//Operators
+    // Arithmetic Operators:- +,-,*,/
+
+    // int a = 2/5;  //answer is 0 as int/int gives answer as int only so instead of 0.4, 0 gets print on the screen
+    // cout<<a<<endl; 
+
+    // float a = 2.0/5; //we have used float so now we get answer as float so we gets output as 0.4
+    // cout<<a<<endl;
+
+    // double b = 2.0/5; //same output as float
+    // cout<<b<<endl;
+
+
+//Relational Operator
+    // int a = 2;
+    // int b = 3;
+
+    // bool first = (a==b);  //0 means false
+    // cout<<first<<endl;
+
+    // bool second = (a<b); //1 means true
+    // cout<<second<<endl;
+    
+    // bool third = (a>b); //0 means false
+    // cout<<third<<endl;
+
+    // bool fourth = (a<=b); //1 means true
+    // cout<<fourth<<endl;
+
+    // bool fifth = (a>=b);//0 means false
+    // cout<<fifth<<endl;
+
+    // bool sixth = (a!=b);//1 means true
+    // cout<<sixth<<endl;
+
+// Negation Operator
+    // int a = 21;
+    // cout << !a << endl; //print 0 flips the output
+
+    // int b = 0;
+    // cout << !b << endl; //prints 1 flips the output
+
+//Logical Operator (&&, ||,)
+
+
+
+    return 0;
+
+}
